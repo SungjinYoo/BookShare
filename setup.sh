dfd9d89a1c8f003453058662b60fb74b91ebae87
Hyungoo Kang <cyluss@gmail.com>
2014-04-27 19:30:19 +0900

    delete south script

--- setup.sh
+++ setup.sh
@@ -17,26 +17,5 @@ halt_when_fail "creating virtualenv"
 pip install -r requirements/base.txt
 halt_when_fail "installing dependency"
 
-`python keygen.py`
-
-pushd bookshare/apps
-APPS=$(find * ! -path . -maxdepth 1 -type d)
-popd
-
-
-for app in $APPS
-do
-    python manage.py schemamigration --initial $app
-    halt_when_fail "schema migration for $app"
-done
-
-python manage.py syncdb
-halt_when_fail "syncdb"
-
-for app in $APPS
-do
-    python manage.py migrate $app
-    halt_when_fail "data migration"
-done
 
 echo -e "\033[32m [SUCCESS] \033[0m Setup done! Follow the remaining steps."
