f767cc87800641c43db0a2d1116c82287a275ba7
Hyungoo Kang <cyluss@gmail.com>
2014-04-29 01:40:16 +0900

    factor out stock

--- bookshare/apps/core/admin.py
+++ bookshare/apps/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import Stock
 
 # Register your models here.
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'owner', 'changed_at', )
+    list_filter = ('status', 'owner', )
+
+admin.site.register(Stock, StockAdmin)
--- bookshare/apps/core/models.py
+++ bookshare/apps/core/models.py
@@ -1,8 +1,32 @@
+#encoding=utf8
+
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+
+from bookshare.apps.books.models import Book
+
+User = get_user_model()
 
 class BookShareModel(models.Model):
     pub_date = models.DateTimeField(_('Published Date'), default=timezone.now, auto_now_add=True)
     class Meta:
         abstract = True
+
+class Stock(models.Model):
+    AVAILABLE = u'available'
+    RENTED = u'rented'
+    
+    STATUS = (
+        (AVAILABLE, u'대여 가능'),
+        (RENTED, u'대여중'),
+    )
+
+    owner = models.ForeignKey(User)
+    book = models.ForeignKey(Book)
+    added_at = models.DateTimeField(auto_now_add=True)
+    changed_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(_(u'상태'), max_length=10,
+                           choices=STATUS,
+                           default=AVAILABLE)
