e54e84de5a9977861f7c1caf63b08fbebe590110
이 태윤 <ok7217@gmail.com>
2014-05-09 17:54:39 +0900

    add login

--- bookshare/urls.py
+++ bookshare/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+from bookshare.views import SignInView
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', 'bookshare.views.index'),
-                       url(r'^signin/$', 'bookshare.views.signin'),
+                       # url(r'^signin/$', 'bookshare.views.signin'),
+                       url(r'^signin/$', SignInView.as_view()),
                        url(r'^signup/$', 'bookshare.views.signup'),
                        url(r'^signout/$', 'bookshare.views.signout'),
                        url(r'^admin/', include(admin.site.urls)),
--- bookshare/views.py
+++ bookshare/views.py
@@ -1,19 +1,25 @@
+# -*- coding:utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseForbidden
-
+from django.views.generic.base import TemplateView
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django import forms
+
+class UserValidationForm(forms.Form):
+    user_id = forms.CharField(max_length=15)
+    password = forms.CharField(max_length=128, min_length=4)
 
 def index(request):
     return render(request, 'bookshare/index.html')
 
-def signin(request):
-    if request.method == 'GET' :
-        return render(request, 'bookshare/signin.html')
-    elif request.method == 'POST' :
-        return render(request, 'bookshare/signin.html')
-    else :
-        return HttpResponseForbidden()
+# def signin(request):
+#     if request.method == 'GET' :
+#         return render(request, 'bookshare/signin.html')
+#     elif request.method == 'POST' :
+#         return render(request, 'bookshare/signin.html')
+#     else :
+#         return HttpResponseForbidden()
 
 def signout(request):
     if request.method == 'GET' :
@@ -23,6 +29,27 @@ def signout(request):
     else :
         return HttpResponseForbidden()
 
+class SignInView(TemplateView):
+    template_name = "bookshare/signin.html"
+    error_msg = u"아이디와 비밀번호를 입력하세요"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        form = UserValidationForm(request.POST)
+        if form.is_valid():
+            user_id = form.cleaned_data['user_id']
+            password = form.cleaned_data['password']
+
+            user = authenticate(user_id=user_id, password=password)
+            if user is not None:
+                if user.check_password(password):
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+        return render(request, self.template_name, {'error_msg':self.error_msg})
+
+    
 def signup(request):
     if request.method == 'GET' :
         return render(request, 'bookshare/signup.html')
