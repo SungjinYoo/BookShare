38911851ef8139d8836359cedf877bd6af53f766
Hyungoo Kang <cyluss@gmail.com>
2014-05-12 12:15:03 +0900

    add book detail

--- bookshare/apps/books/views.py
+++ bookshare/apps/books/views.py
@@ -10,9 +10,9 @@ def index(request):
     return render(request, "index.html", context)
 
 
-def book(request, book_id):
-    book = models.Book.objects.get(id=book_id)
-    context = {
-        "book": book
-    }
-    return render(request, "book.html", context)
+from django.views.generic.detail import DetailView
+
+from models import Book
+
+class BookDetailView(DetailView):
+    model = Book
--- bookshare/urls.py
+++ bookshare/urls.py
@@ -1,19 +1,20 @@
 from django.conf.urls import patterns, include, url
-
-from bookshare.views import MyPageView
-
 from django.contrib import admin
 
-from bookshare.views import SignInView, SignUpView
+from bookshare.views import MyPageView, SignInView, SignUpView
 from bookshare.apps.core import urls
+from bookshare.apps.books.views import BookDetailView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', 'bookshare.views.index'),
-                       url(r'^signin/$', SignInView.as_view()),
-                       url(r'^signup/$', SignUpView.as_view()),
-                       url(r'^signout/$', 'bookshare.views.signout'),
-                       url(r'^me/$', MyPageView.as_view()),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^test/', include(urls.urlpatterns, namespace="core")),
+   url(r'^$', 'bookshare.views.index'),
+   url(r'^signin/$', SignInView.as_view()),
+   url(r'^signup/$', SignUpView.as_view()),
+   url(r'^signout/$', 'bookshare.views.signout'),
+   url(r'^me/$', MyPageView.as_view()),
+   url(r'^admin/', include(admin.site.urls)),
+   url(r'^test/', include(urls.urlpatterns, namespace="core")),
+   url(r'^books/(?P<pk>[0-9]+)$', BookDetailView.as_view(), name='article-detail'),
 )
+
