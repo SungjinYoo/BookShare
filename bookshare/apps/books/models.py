ad842da83c395167b0dfb2d64f4b97fbb4f1ef52
Hyungoo Kang <cyluss@gmail.com>
2014-08-12 15:19:20 +0900

    drop columns

--- bookshare/apps/books/models.py
+++ bookshare/apps/books/models.py
@@ -10,28 +10,11 @@ from django.utils.translation import ugettext_lazy as _
 isbn_validator = RegexValidator(regex='^\d{13}$', message='Length has to be 13', code='nomatch')
 
 class Course(models.Model):
-    FIRST = u'first'
-    SUMMER = u'summer'
-    SECOND = u'second'
-    WINTER = u'winter'
-
-    SEMESTERS = (
-        (FIRST, u'1학기'),
-        (SUMMER, u'여름'),
-        (SECOND, u'2학기'),
-        (WINTER, u'겨울')
-    )
-
     title = models.CharField(max_length=20)
-    department = models.CharField(max_length=20)
     year = models.IntegerField()
-    semester = models.CharField(_(u'학기'), max_length=10,
-                                choices=SEMESTERS,
-                                default=FIRST)
     
-
     def __unicode__(self):
-        return u"{} - {} {}".format(self.title, self.year, self.semester)
+        return u"{} - {} {}".format(self.title, self.year)
 
 class Book(models.Model):
     courses = models.ManyToManyField(Course)
