9f4480e725a0dc3ff59dd2db4e2e003ca088938d
Hyungoo Kang <cyluss@gmail.com>
2014-08-21 13:31:33 +0900

    refactor

--- bookshare/static/js/console.book.js
+++ bookshare/static/js/console.book.js
@@ -1,15 +1,30 @@
 
-function isValid(isbn) {
+function isValidISBN(isbn) {
     return isbn.match(/^\d{13}$/) !== null;
 }
 
-function searchBook(isbn) {
-    return $.getJSON("http://apis.daum.net/search/book?callback=?",
-            {q: isbn,
-             searchType: "isbn",
-             output: "json",
-             apikey: "e687f4cfe38cb4916caa8240e7c8588a6269c21c"})
+function searchBookDaum(isbn) {
+    var promise = jQuery.Deferred();
 
+    $.getJSON("http://apis.daum.net/search/book?callback=?",
+        {q: isbn,
+         searchType: "isbn",
+         output: "json",
+         apikey: "e687f4cfe38cb4916caa8240e7c8588a6269c21c"}
+    ).done(function(content) {
+        promise.resolve(content.channel.item[0].title);
+    });
+
+    return promise;
+}
+
+function coverImageKyobo(isbn) {
+    return "http://image.kyobobook.co.kr/images/book/xlarge/" + isbn.substr(isbn.length - 3) + "/x" + isbn + ".jpg"
+}
+
+BookStore = {
+    maybeGetTitle: searchBookDaum,
+    getCoverImage: coverImageKyobo
 }
 
 $(function() {
@@ -17,17 +32,16 @@ $(function() {
         var self = $(this);
         var isbn = self.val();
 
-        if (isValid(isbn)) {
+        if (isValidISBN(isbn)) {
             var form = $(self.closest("form"));
             
             var cover_url_field = form.find("#id_cover_url");
-            cover_url_field.val("http://image.kyobobook.co.kr/images/book/xlarge/" + isbn.substr(isbn.length - 3) + "/x" + isbn + ".jpg");
+            cover_url_field.val(BookStore.getCoverImage(isbn));
             cover_url_field.trigger('input');
 
             var title_field = form.find("#id_title");
-
-            searchBook(isbn).done(function(content) {
-                title_field.val(content.channel.item[0].title);
+            BookStore.maybeGetTitle(isbn).done(function(title) {
+                title_field.val(title);
             });
         }
     });
