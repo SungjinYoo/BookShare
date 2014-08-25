60d26aefcded4f399fd3e47bdde191ee1356d03c
Hyungoo Kang <cyluss@gmail.com>
2014-08-25 23:35:37 +0900

    add production setting

--- bookshare/settings/production.py
+++ bookshare/settings/production.py
@@ -20,7 +20,6 @@ def get_env_variable(var_name):
 
 SECRET_KEY = get_env_variable('BOOKSHARE_SECRET_KEY')
 
-DEBUG = True
 TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['*']
 
@@ -103,7 +102,7 @@ TEMPLATE_DIRS = (
 
 AUTH_USER_MODEL = 'users.User'
 
-MEDIA_ROOT = PROJECT_ROOT / '..' / 'media'
+MEDIA_ROOT = PROJECT_ROOT / .. / 'media'
 
 STATIC_ROOT = PROJECT_ROOT / '..' / 'static'
 
