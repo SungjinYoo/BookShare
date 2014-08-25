46bff59ff96d8f115fe77cf671b1bf2cae2362d8
Hyungoo Kang <cyluss@gmail.com>
2014-08-25 23:57:57 +0900

    fix subtle mistakes

--- bookshare/settings/base.py
+++ bookshare/settings/base.py
@@ -103,7 +103,7 @@ TEMPLATE_DIRS = (
 
 AUTH_USER_MODEL = 'users.User'
 
-MEDIA_ROOT = PROJECT_ROOT / '..' / 'media'
+MEDIA_ROOT = PROJECT_ROOT / 'media'
 
 STATIC_ROOT = PROJECT_ROOT / '..' / 'static'
 
