a62707f1da9feea43eb46c11b53feade4ee6a501
cheyuni <ok7217@gmail.com>
2014-09-13 16:56:14 +0900

    remove unused column

--- bookshare/apps/books/admin.py
+++ bookshare/apps/books/admin.py
@@ -6,8 +6,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', "isbn", "num_available_stocks")
 
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'year', )
-    list_filter = ('year', )
+    list_display = ('title',)
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Course, CourseAdmin)
